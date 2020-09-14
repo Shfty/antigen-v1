7e3e22cfcf637be210f0215677094e4e45fc3531
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2020-09-14 07:08:02 +0100

    Iterate

--- antigen/src/ecs/mod.rs
+++ antigen/src/ecs/mod.rs
@@ -9,7 +9,6 @@ pub mod system_runner;
 
 use std::collections::HashMap;
 
-use system_runner::SingleThreadedSystemRunner;
 pub use assemblage::{Assemblage, AssemblageID};
 pub use component::{ComponentDataID, ComponentDebugTrait, ComponentID, ComponentTrait};
 pub use component_storage::{ComponentStorage, HeapComponentStorage};
@@ -17,6 +16,7 @@ pub use entity::EntityID;
 pub use entity_component_database::EntityComponentDatabase;
 use entity_component_database::SingleThreadedDatabase;
 pub use system::{SystemError, SystemTrait};
+use system_runner::SingleThreadedSystemRunner;
 pub use system_runner::SystemRunner;
 
 type EntityCreateCallback<T> = fn(&mut T, EntityID, Option<&str>);
@@ -25,10 +25,18 @@ type ComponentCreateCallback<T> = fn(&mut T, ComponentID, &str, &str);
 pub struct EntityComponentSystem<'a> {
     component_storage: HeapComponentStorage<'a>,
     entity_component_directory: SingleThreadedDatabase<'a>,
-    system_runner: SingleThreadedSystemRunner<'a, SingleThreadedDatabase<'a>>
+    system_runner: SingleThreadedSystemRunner<'a, SingleThreadedDatabase<'a>>,
 }
 
 impl<'a> EntityComponentSystem<'a> {
+    fn new() -> Self {
+        EntityComponentSystem {
+            component_storage: HeapComponentStorage::new(),
+            entity_component_directory: SingleThreadedDatabase::new(),
+            system_runner: SystemRunner::new(),
+        }
+    }
+
     // Methods to keep
     fn is_valid_entity(&self, entity_id: &EntityID) -> bool {
         self.entity_component_directory.is_valid_entity(entity_id)
@@ -102,41 +110,47 @@ impl<'a> EntityComponentSystem<'a> {
     }
 
     fn get_entity_by_predicate(&self, predicate: impl Fn(&EntityID) -> bool) -> Option<EntityID> {
-        self.entity_component_directory.get_entity_by_predicate(predicate)
+        self.entity_component_directory
+            .get_entity_by_predicate(predicate)
     }
 
     fn get_entities_by_predicate(&self, predicate: impl Fn(&EntityID) -> bool) -> Vec<EntityID> {
-        self.entity_component_directory.get_entities_by_predicate(predicate)
+        self.entity_component_directory
+            .get_entities_by_predicate(predicate)
     }
 
     fn get_components_by_predicate(
         &self,
         predicate: impl Fn(&ComponentID) -> bool,
     ) -> Vec<ComponentID> {
-        self.entity_component_directory.get_components_by_predicate(predicate)
+        self.entity_component_directory
+            .get_components_by_predicate(predicate)
     }
 
     fn entity_has_component<T: ComponentTrait + 'static>(&self, entity_id: &EntityID) -> bool {
-        self.entity_component_directory.entity_has_component::<T>(entity_id)
+        self.entity_component_directory
+            .entity_has_component::<T>(entity_id)
     }
 
     fn entity_has_component_by_id(&self, entity_id: &EntityID, component_id: &ComponentID) -> bool {
-        self.entity_component_directory.entity_has_component_by_id(entity_id, component_id)
+        self.entity_component_directory
+            .entity_has_component_by_id(entity_id, component_id)
     }
 
     fn get_entity_component<T: ComponentTrait + 'static>(
         &self,
         entity_id: EntityID,
     ) -> Result<&T, String> {
-        self.entity_component_directory.get_entity_component(entity_id)
+        self.entity_component_directory
+            .get_entity_component(entity_id)
     }
 
     fn get_entity_component_mut<T: ComponentTrait + 'static>(
         &mut self,
         entity_id: EntityID,
     ) -> Result<&mut T, String> {
-        self.entity_component_directory.get_entity_component_mut(entity_id)
-
+        self.entity_component_directory
+            .get_entity_component_mut(entity_id)
     }
 
     fn get_entity_component_data_id(
@@ -144,7 +158,8 @@ impl<'a> EntityComponentSystem<'a> {
         entity_id: &EntityID,
         component_id: &ComponentID,
     ) -> Result<ComponentDataID, String> {
-        self.entity_component_directory.get_entity_component_data_id(entity_id, component_id)
+        self.entity_component_directory
+            .get_entity_component_data_id(entity_id, component_id)
     }
 
     fn add_component_to_entity<T: ComponentTrait + ComponentDebugTrait + 'static>(
@@ -152,14 +167,16 @@ impl<'a> EntityComponentSystem<'a> {
         entity_id: EntityID,
         component_data: T,
     ) -> Result<&mut T, String> {
-        self.entity_component_directory.add_component_to_entity(entity_id, component_data)
+        self.entity_component_directory
+            .add_component_to_entity(entity_id, component_data)
     }
 
     fn remove_component_from_entity<T: ComponentTrait + ComponentDebugTrait + 'static>(
         &mut self,
         entity_id: EntityID,
     ) -> Result<(), String> {
-        self.entity_component_directory.remove_component_from_entity::<T>(entity_id)
+        self.entity_component_directory
+            .remove_component_from_entity::<T>(entity_id)
     }
 
     fn get_component_data(
@@ -169,11 +186,15 @@ impl<'a> EntityComponentSystem<'a> {
         self.component_storage.get_component_data(component_data_id)
     }
 
-    fn register_system(&mut self, name: &str, system: &'a mut dyn SystemTrait<SingleThreadedDatabase<'a>>) {
+    fn register_system(
+        &mut self,
+        name: &str,
+        system: &'a mut dyn SystemTrait<SingleThreadedDatabase<'a>>,
+    ) {
         self.system_runner.register_system(name, system)
     }
 
     fn run(&mut self) -> Result<(), SystemError> {
-        self.system_runner.run()
+        self.system_runner.run(&mut self.entity_component_directory)
     }
 }

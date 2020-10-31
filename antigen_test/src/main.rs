54adf4eba4e0d80d0e8590c6335a5c984f2549b3
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2020-10-31 18:53:25 +0000

    Refactor main to return a Result

--- antigen_test/src/main.rs
+++ antigen_test/src/main.rs
@@ -9,33 +9,27 @@ use antigen::{
     entity_component_system::{EntityComponentSystem, SystemError},
 };
 
-fn main() {
-    if let Err(err) = main_internal() {
-        match err {
-            SystemError::Err(err) => {
-                eprintln!("{}", err);
-                std::process::exit(1)
-            }
-            SystemError::Quit => std::process::exit(0),
-        }
-    }
-}
-
-fn main_internal() -> Result<(), SystemError> {
+fn main() -> Result<(), SystemError> {
+    // Create and populate ECS
     let mut ecs = EntityComponentSystem::default();
-
     scenes::antigen_debug_scene::system_assembler(Default::default()).finish(&mut ecs);
     scenes::antigen_debug_scene::entity_assembler(Default::default())
         .finish(ecs.get_component_store());
 
-    // Main loop
+    // Run main loop
     let frame_time_target = Duration::from_secs_f32(1.0 / 60.0);
     loop {
         let main_loop_profiler = Profiler::start();
-        ecs.run()?;
-        let delta = main_loop_profiler.finish();
+
+        if let Err(err) = ecs.run() {
+            match err {
+                SystemError::Err(_) => return Err(err),
+                SystemError::Quit => return Ok(()),
+            }
+        }
 
         // Sleep if framerate target is exceeded - prevents deadlock when pancurses stops being able to poll input after window close
+        let delta = main_loop_profiler.finish();
         if delta < frame_time_target {
             let delta = frame_time_target - delta;
             std::thread::sleep(delta);
